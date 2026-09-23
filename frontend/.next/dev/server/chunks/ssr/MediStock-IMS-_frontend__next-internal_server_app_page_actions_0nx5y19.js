7bf80021b3859417b723f7b9e89ec139bdeb595d
TR-Mokwena <tshepisodev@gmail.com>
2026-09-23 14:53:48 +0200

    Fix gitignore encoding, untrack .next build output

--- frontend/.next/dev/server/chunks/ssr/MediStock-IMS-_frontend__next-internal_server_app_page_actions_0nx5y19.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/MediStock-IMS-/frontend/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=MediStock-IMS-_frontend__next-internal_server_app_page_actions_0nx5y19.js.map
--- frontend/.next/dev/static/chunks/MediStock-IMS-_frontend_1anvha4._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0lz_zbq._.js","static/chunks/0t0j_next_dist_compiled_next-devtools_index_0f1m0gv.js","static/chunks/0t0j_next_dist_compiled_react-dom_1qnv-2g._.js","static/chunks/0t0j_next_dist_compiled_react-server-dom-turbopack_0pw-s7e._.js","static/chunks/0t0j_next_dist_compiled_0pf77_b._.js","static/chunks/0t0j_next_dist_client_0p5f-ch._.js","static/chunks/0t0j_next_dist_06o21fj._.js","static/chunks/0t0j_@swc_helpers_cjs_0mzswn8._.js"],
-    source: "entry"
-});
